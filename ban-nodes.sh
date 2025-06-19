c73a0c435ad479b908559dbdf3b4a21e409f314e
Martin Biolek <43006943+mbio16@users.noreply.github.com>
2025-06-19 08:57:27 +0200

    Create ban-nodes.sh

---
+++ ban-nodes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ban LinkingLion
+bitcoin-cli setban 162.218.65.0/24    add 31536000
+bitcoin-cli setban 209.222.252.0/24   add 31536000
+bitcoin-cli setban 91.198.115.0/24    add 31536000 
+bitcoin-cli setban 2604:d500:4:1::/64 add 31536000
